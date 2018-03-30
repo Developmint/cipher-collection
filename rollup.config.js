397358198c34044e5c336277e9d5e322e5152f5e
Alexander Lichter <manniL@gmx.net>
2018-03-30 19:40:18 +0200

    build: Configure Rollup

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import babel from 'rollup-plugin-babel'
+import pkg from './package.json'
+
+const plugins = [
+  babel({
+    presets: [
+      [
+        'env',
+        {
+          modules: false
+        }
+      ]
+    ],
+    plugins: ['external-helpers', 'transform-object-rest-spread'],
+    babelrc: false
+  })
+]
+
+export default [
+  // browser-friendly UMD build
+  {
+    input: 'src/index.js',
+    output: {
+      name: 'cipher-collection',
+      file: pkg.browser,
+      format: 'umd'
+    },
+    plugins
+  },
+  {
+    input: 'src/index.js',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins
+  }
+]
