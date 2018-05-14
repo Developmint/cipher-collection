c193574344f2636c8c37593fc839fd9352b63c24
Alexander Lichter <manniL@gmx.net>
2018-05-14 12:47:28 +0200

    refactor: improve substitute array mapping

--- src/helpers/substitute.js
+++ src/helpers/substitute.js
@@ -4,8 +4,12 @@ export const substitute = (input, options = {}) => {
   const mappingEntries = Object.entries(options.mapping)
 
   return input.replace(/./g, x => {
-    const transformedEntries = options.caseSensitive ? mappingEntries : mappingEntries.map(([k, v]) => isLowerCase(x) ? [k.toLowerCase(), v.toLowerCase()] : [k.toUpperCase(), v.toUpperCase()])
-    const foundMapping = transformedEntries.find(([k, v]) => k === x || v === x)
+    const transformedEntries = options.caseSensitive
+      ? mappingEntries
+      : mappingEntries.map(array => array.map(c => isLowerCase(x) ? c.toLowerCase() : c.toUpperCase()))
+
+    const foundMapping = transformedEntries.find(kvArray => kvArray.includes(x))
+
     return !foundMapping ? x : foundMapping[0] === x ? foundMapping[1] : foundMapping[0]
   })
 }
